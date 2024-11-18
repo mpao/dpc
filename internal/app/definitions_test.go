ed1e6b1140015a7ee216a214423c3d9a9ec45869
Marco Paoletti <marco1.paoletti@telecomitalia.it>
2024-11-18 15:02:54 +0100

    rinomina files in package app

--- internal/app/definitions_test.go
+++ internal/app/definitions_test.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -28,6 +29,29 @@ func TestParseDates(t *testing.T) {
 			if err != nil {
 				got = err.Error()
 			}
+			assert.Equal(t, test.want, got)
+		})
+	}
+}
+
+func TestDayEqual(t *testing.T) {
+	cases := []struct {
+		name  string
+		date1 string
+		date2 string
+		want  bool
+	}{
+		{date1: "20240101_0100", date2: "20240101_0100", want: true},
+		{date1: "20240101_0100", date2: "20240101_0500", want: true},
+		{date1: "20240101_0100", date2: "20240102_0100", want: false},
+		{date1: "20230101_0100", date2: "20240101_0100", want: false},
+		{date1: "20240201_0100", date2: "20240101_0100", want: false},
+	}
+	for _, test := range cases {
+		t.Run(test.name, func(t *testing.T) {
+			d1, _ := time.Parse("20060102_1504", test.date1)
+			d2, _ := time.Parse("20060102_1504", test.date2)
+			got := DayEqual(d1, d2)
 			assert.Equal(t, test.want, got)
 		})
 	}
