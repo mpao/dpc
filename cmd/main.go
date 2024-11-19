a697b5c9fdd21a29dfc460b223e9ab725e6ab05d
Marco Paoletti <marco1.paoletti@telecomitalia.it>
2024-11-19 14:16:18 +0100

    Revert "aggiunge messaggio di operazioni in corso"
    This reverts commit ea6354b3b83292611500093e60425cc7b6b73193.

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
 )
 
 var (
@@ -12,30 +11,8 @@ var (
 )
 
 func main() {
-	prefix := "avvio operazioni: "
-	s := make(chan string)
-	go func() {
-		go spinner(prefix)
-		if err := root.Execute(); err != nil {
-			fmt.Printf("\rERRORE: %v\n", err)
-			os.Exit(1)
-		}
-		s <- "fatto!"
-	}()
-	fmt.Printf("\r%s%v\n", prefix, <-s)
-}
-
-func spinner(msg string) {
-	for {
-		for _, r := range `-\|/` {
-			// \r, carriage return posiziona il cursore ad inizio riga
-			// e ricomincia a scrivere. Questa operazione simula la riscrittura
-			// degli ultimi caratteri se si utilizza il medesimo prefisso.
-			// prova a eliminare \r per vedere cosa intendo
-			fmt.Printf("\r%s%c", msg, r)
-			// per dare il senso rotatorio, scrivi i caratteri con
-			// un intervallo di XXms uno dall'altro
-			time.Sleep(70 * time.Millisecond)
-		}
+	if err := root.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
